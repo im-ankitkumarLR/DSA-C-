b7409075625c0d8166bf78d490dd22cf2906d80e
aknasa <aknasa12@gmail.com>
2022-02-02 11:26:16 +0530

    P-5

--- patternscode.cc
+++ patternscode.cc
@@ -3,21 +3,22 @@ using namespace std;
 int main()
 {
     
-
-    //   A 
-    //   B C 
-    //   D E F
-    //   G H I J 
+// D
+// CD
+// BCD
+// ABCD
     int i, j, n;
-    char ch = 65;
+    // char ch = 74;
     cout << "Enter the number" << endl;
     cin >> n;
     for (i = 1; i <= n; i++)
     {  
+        char k='A'+n-i;
         for (j= 1; j<=i; j++)
         {
-            cout<<ch;
-            ch++;
+            cout<<k;
+            k++;
+        
         }
             
         cout<<endl;
