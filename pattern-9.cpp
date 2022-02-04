73bead8686e24015f2300b971686b08766bc2c7a
aknasa <aknasa12@gmail.com>
2022-02-04 21:48:53 +0530

    P-9

--- pattern-9.cpp
+++ pattern-9.cpp
@@ -2,28 +2,27 @@
 using namespace std;
 int main()
 {
-    //   1111
-    //    222
-    //     33
-    //      4
+    //      1
+    //     22
+    //    333
+    //   4444
     int i,j,n;
     // char ch = 74;
     cout << "Enter the number" << endl;
     cin >> n;
     for (i = 1; i <= n; i++)
     {  
-            // for spaces
-            for (j= 1; j<=i-1; j++)
-            {
-                cout<<" ";
-            }
-        // for stars
-        for (j= 1; j<=n-i+1; j++)
+        // for spaces
+        for (j= 1; j<=n-i; j++)
         {
-            cout<<i;
+            cout<<" ";
         
-            // k++;
         }
+            // for  numbers
+            for (j= 1; j<=i; j++)
+            {
+                cout<<i;
+            }
             
         cout<<endl;
     }
