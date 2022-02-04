96b067d6ceeb21967076c111732723db8c936e44
aknasa <aknasa12@gmail.com>
2022-02-04 22:58:09 +0530

    Tough2pattern-12

---
+++ toughpattern-12.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+         
+    int i,j,n;
+    
+    cout << "Enter the number" << endl;
+    cin >> n;
+    for (i = 1; i <= n; i++)
+    {  
+            // for  numbers
+            for (j= 1; j<=n-i+1; j++)
+
+            {
+                cout<<j;
+            }
+        // for spaces
+        for (j= 1; j<=i-1; j++)
+        {
+            cout<<"*";
+        
+        }
+        // for spaces
+        for (j= 1; j<=i-1; j++)
+        {
+            cout<<"*";
+        
+        }
+         // for  numbers
+             
+            for (j= 1; j<=n-i+1;j++)
+
+            {
+                cout<<j;
+                
+            }
+            
+        cout<<endl;
+    }
+}
