7679992186d7cdab193c4be8228b3fc7710f16ae
aknasa <aknasa12@gmail.com>
2022-02-04 21:32:03 +0530

    P-8

---
+++ pattern-8.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+//   ****
+//    ***
+//     **
+//      *
+    int i,j,n;
+    // char ch = 74;
+    cout << "Enter the number" << endl;
+    cin >> n;
+    for (i = 1; i <= n; i++)
+    {  
+            // for spaces
+            for (j= 1; j<=i-1; j++)
+            {
+                cout<<" ";
+            }
+        // for stars
+        for (j= 1; j<=n-i+1; j++)
+        {
+            cout<<"*";
+        
+            // k++;
+        }
+            
+        cout<<endl;
+    }
+
+}
