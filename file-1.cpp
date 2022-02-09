a28bbb9ac7fc429cbceb1c94dd8d84de7206eea5
aknasa <aknasa12@gmail.com>
2022-02-09 12:18:36 +0530

    P-1

---
+++ file-1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int n,i,sum=0;
+    cout<<"Enter the any number"<<endl;
+    cin>>n;
+    for (i =1; i <=n; i++)
+    {
+       sum=sum+i;
+    }
+   cout<<sum<<endl;
+    
+
+}
